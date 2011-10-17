228e2a458ce1717c6f9fd5f815384823d8e3b864
LLynx <l.lynx@ymail.com>
2011-10-17 20:04:41 +0200

    Started to implement terminal IO via Term::ReadLine

--- SVocTrainer.pl
+++ SVocTrainer.pl
@@ -35,6 +35,11 @@ if ( 0 ) # <<= insert here the wanted value!
 use warnings;
 use strict;
 
+use Term::ReadLine;
+
+
+my $term = Term::ReadLine->new('SVocTrainer');
+my $OUT = $term->OUT || \*STDOUT;
 my $inp;
 my $ask_off;
 my $ans_off;
@@ -46,6 +51,7 @@ my @order;
 my @mode;
 my $num = 0;
 my $numinfile = '';
+
 
 sub readnchomp ## this subroutine reads and chomps at the same time via STDIN
 {
@@ -70,17 +76,19 @@ sub contains( $@ ) ## returns 1 if the second parameter as array contains the fi
 
 sub contains_num( $@ ) # same as contains(), but with numbers
 {
-        my $elem = shift @_;
-        my @list = @_;
-        for my $i ( 0..scalar @list - 1 )
-        {
-                if ( $list[$i] == $elem )
-                {
-                        return 1;
-                }
-        }
-        return 0;
+	my $elem = shift @_;
+	my @list = @_;
+	for my $i ( 0..scalar @list - 1 )
+	{
+		if ( $list[$i] == $elem )
+		{
+			return 1;
+		}
+	}
+	return 0;
 }
+
+
 ######## Begin of actual program
 
 for my $i ( 1 .. scalar @ARGV - 1 )
@@ -93,13 +101,13 @@ my $m = $ARGV[1]; # $ARGV[1] is the mode (t/w/d)
 
 if ( not ( ( $l == 4 and $m eq 't' ) or ( $l == 3 and $m eq 'd' ) or ( $l == 2 and $m eq 'w' ) ) ) # If the number of arguments isn't correct (in relation to the chosen mode), ask again
 {
-	print "Wrong number of parameters! Please type all arguments correct again: > ";
+	print $OUT "Wrong number of parameters! Please type all arguments correct again: > ";
 	$inp = readnchomp();
 	@mode = split ' ',$inp;
 	$ARGV[0] = shift @mode;
 } elsif ( $ARGV[1] eq 't' and $ARGV[3] eq 'b' ) # Training mode and bidirectionality isn't good.
 {	
-	print "Wrong number of parameters! Please type all arguments correct again: > ";
+	print $OUT "Wrong number of parameters! Please type all arguments correct again: > ";
 	$inp = readnchomp();
 	@mode = split ' ',$inp;
 	$ARGV[0] = shift @mode;
@@ -119,18 +127,18 @@ if ( $mode[0] ne 'w' ) # only read vocabulary from file if another mode than "wr
 		if ( $inp =~ m/^\s*([^=#]*[^=#\s])\s*=\s*([^=#]*[^=#\s]).*$/ )
 		{
 #                                      l1 l2
-                        $vocs[$num] = [$1,$2];
-                        $num++;
+			$vocs[$num] = [$1,$2];
+			$num++;
 		}
 	}
 
 	close $vocFile;
-	print "\n$num correct records read and processed.\n";
+	print $OUT "\n$num correct records read and processed.\n";
 }
 
 if ( $mode[0] eq 't' )
 {
-	print "\nmode: vocabulary test\n";
+	print $OUT "\nmode: vocabulary test\n";
 
 	# generation of the @order array. This algorithm makes sure that the generation of the order is fast and secure
 	if ( $mode[2] eq 'l' )
@@ -140,7 +148,7 @@ if ( $mode[0] eq 't' )
 			$order[$i] = $i;
 		}
 
-		print "order: linear\n";
+		print $OUT "order: linear\n";
 	}
 	elsif ( $mode[2] eq 'r' )
 	{
@@ -159,39 +167,39 @@ if ( $mode[0] eq 't' )
 			splice( @a, $rand, 1 );
 		}
 
-		print "order: random\n";
+		print $OUT "order: random\n";
 	}
 
 	if ( $mode[1] == 2 )
 	{
-		print "direction: l1 -> l2\n\n";
-                ($ask_off,$ans_off) = (0,1);
+		print $OUT "direction: l1 -> l2\n\n";
+		($ask_off,$ans_off) = (0,1);
 	}
 	elsif ( $mode[1] == 1 )
 	{
-		print "direction: l2 -> l1\n\n";
-                ($ask_off,$ans_off) = (1,0);
+		print $OUT "direction: l2 -> l1\n\n";
+		($ask_off,$ans_off) = (1,0);
 	}
 
 
-	for ( my $i = 0; $i < $num; ++$i ) # has to be this type of loop because of the backstep in case of a false answer
+	for ( my $i = 0; $i < $num; ++$i ) # has to be this type of loop because of the backstep in case of a wrong answer
 	{
 		$ix = $order[$i];
 		if ( $mode[2] eq 'r' )
 		{
 			$numinfile = '(#'. ($ix + 1) . ')';
 		} 
-		print( ( $i+1 ) . "/$num $numinfile: $vocs[$ix]->[$ask_off] ?  > " );
-		$inp = readnchomp();
+		$inp = $term->readline( ( $i+1 ) . "/$num $numinfile: $vocs[$ix]->[$ask_off] ?  > " );
+		$term->addhistory($inp);
 
 ###### OPcodes exec.
 
 		if ( $inp eq '!status' )
 		{
 			$i == 0 ? $i = 1 : 0; # necessary to avoid illegal division by zero in the next two lines
-			printf "You gave " . ( (scalar @wrongList) - 1 ) . " wrong answers which are %d %%\n",( (scalar @wrongList - 1) / $i ) * 100 ;
-			printf "You gave " . ( $i - ( (scalar @wrongList) - 1 ) ) . " correct answers which are %d %%\n",( ( $i - ( scalar @wrongList - 1 ) ) / $i ) * 100 ;
-			print "You have to answer " . ( $num - $i ) . " words ($num words were read)\n\n";
+			printf($OUT "You gave " . ( (scalar @wrongList) - 1 ) . " wrong answers which are %d %%\n",( (scalar @wrongList - 1) / $i ) * 100);
+			printf($OUT "You gave " . ( $i - ( (scalar @wrongList) - 1 ) ) . " correct answers which are %d %%\n",( ( $i - ( scalar @wrongList - 1 ) ) / $i ) * 100);
+			print $OUT "You have to answer " . ( $num - $i ) . " words ($num words were read)\n\n";
 			$inp = "!OPCODE_ASKED";# See some lines above. This value marks an answer not as 'wrong' which avoids the increment of the fail-counter
 			--$i;# If !status was called, repeat the word
 		}
@@ -200,48 +208,48 @@ if ( $mode[0] eq 't' )
 
 		if ( $inp eq '!edit' )
 		{
-			print("You chose edit mode. If you save the changed vocabulary, all comments will be lost! Press ^C to abort immediately.\nIf you type nothing, the word will be untouched.\n");
-			
-			print("Change: $vocs[$ix]->[$ask_off] ? > ");
-			$inp = readnchomp();
+			print $OUT "You chose edit mode. If you save the changed vocabulary, all comments will be lost! Press ^C to abort immediately.\nIf you type nothing, the word will be untouched.\n";
+
+			$inp = $term->readline("Change: $vocs[$ix]->[$ask_off] ? > ");
+			$term->addhistory($inp);
 			$vocs[$ix]->[$ask_off] = $inp if ( $inp ne '' );
-			
-			print("Change: $vocs[$ix]->[$ans_off] ? > ");
-			$inp = readnchomp();
+
+			$inp = $term->readline("Change: $vocs[$ix]->[$ans_off] ? > ");
+			$term->addhistory($inp);
 			$vocs[$ix]->[$ans_off] = $inp if ( $inp ne '' );
-			
+
 			open(my $writeFile,">","$ARGV[0]");
-			
-			print($writeFile "### This file was created by SVocTrainer (c) 2010, 2011 Der Messer & LLynx\n");
+
+			print $writeFile "### This file was created by SVocTrainer (c) 2010, 2011 Der Messer & LLynx\n";
 
 			for my $j ( 0..($num-1) )
 			{
-                                        print($writeFile "$vocs[$j]->[0]=$vocs[$j]->[1]\n");
+				print $writeFile "$vocs[$j]->[0]=$vocs[$j]->[1]\n";
 			}
-			
+
 			close($writeFile);
-			
+
 			$inp = "!OPCODE_ASKED";
 			$i--;
 		}			
-		
+
 ########################################
 
 		if ( $inp eq '!exit' )
 		{
-			print "\nAborted on request!\n\n";
+			print $OUT "\nAborted on request!\n\n";
 			exit;
 		}
 
 
 		if ( contains( lc( $inp ), split( '/', lc( $vocs[$ix]->[$ans_off] ) ) ) || $inp eq $correct)
 		{
-			print "Correct!\n\n";
+			print $OUT "Correct!\n\n";
 
 ###### /Opcodes
 		} elsif ( $inp ne "!OPCODE_ASKED" ) 
 		{
-			print "Wrong! Correct was: $vocs[$ix]->[$ans_off]\n";
+			print $OUT "Wrong! Correct was: $vocs[$ix]->[$ans_off]\n";
 
 			if ( ( $wrongList[0] != $ix ) )
 			{
@@ -254,38 +262,38 @@ if ( $mode[0] eq 't' )
 	pop @wrongList;
 
 	my $correctNum = $num - scalar @wrongList;
-	printf( "\nYou knew %i out of %i, which are %i percent.\n\n", $correctNum, $num, $correctNum / $num * 100 );
+	printf($OUT "\nYou knew %i out of %i, which are %i percent.\n\n", $correctNum, $num, $correctNum / $num * 100);
 
 	while (scalar @wrongList > 0)
 	{
 		for ( my $i = scalar @wrongList - 1; $i >= 0; --$i )
 		{
 			$ix = $wrongList[$i];
-			print "$vocs[$ix]->[$ask_off] ?  > ";
-			$inp = readnchomp();
+			$inp = $term->readline("$vocs[$ix]->[$ask_off] ?  > ");
+			$term->addhistory($inp);
 			if ( contains( lc( $inp ), split( '/', lc( $vocs[$ix]->[$ans_off] ) ) ) )
 			{
-				print "Correct!\n";
+				print $OUT "Correct!\n";
 				splice @wrongList, $i, 1;
 			}
 			else
 			{
-				print "Wrong! Correct was: $vocs[$ix]->[$ans_off]\n";
+				print $OUT "Wrong! Correct was: $vocs[$ix]->[$ans_off]\n";
 			}
 		}
 	}
 }
 elsif ( $mode[0] eq 'd' )
 {
-	print "\nmode: dictionary look-up (languages: $mode[1])\n\nEnter nothing to leave the program.";
+	print $OUT "\nmode: dictionary look-up (languages: $mode[1])\n\nEnter nothing to leave the program.";
 
 	while ( 1 ) # loop is terminated with last
 	{
-                my @found;
-		print "\n\nEnter a regular expression to search for: > ";
-		$inp = readnchomp();
+		my @found;
+		$inp = $term->readline("\n\nEnter a regular expression to search for: > ");
+		$term->addhistory($inp);
 		last if ( $inp eq '' ); # exit loop if input was empty
-		print "\nResults:\n";
+		print $OUT "\nResults:\n";
 
 		my $count = 0;
 
@@ -296,8 +304,8 @@ elsif ( $mode[0] eq 'd' )
 				if ( $vocs[$i]->[0] =~ m/$inp/ )
 				{
 					++$count;
-                                        push @found,$i;
-					print "$vocs[$i]->[0] = $vocs[$i]->[1]\n";
+					push @found,$i;
+					print $OUT "$vocs[$i]->[0] = $vocs[$i]->[1]\n";
 				}
 			}
 		}
@@ -308,25 +316,25 @@ elsif ( $mode[0] eq 'd' )
 				if ( $vocs[$i]->[1] =~ m/$inp/ and not contains_num($i,@found) )
 				{
 					++$count;
-					print "$vocs[$i]->[0] = $vocs[$i]->[1]\n";
+					print $OUT "$vocs[$i]->[0] = $vocs[$i]->[1]\n";
 				}
 			}
 		}
-		print "\nFound $count matches\n";
+		print $OUT "\nFound $count matches\n";
 	}
 }
 
-print "\n";
+print $OUT "\n";
 
 if ( $mode[0] eq 'w' )
 {
-	print "mode: write\nSave the vocabulary and quit SVT by typing an empty line!\n";
+	print $OUT "mode: write\nSave the vocabulary and quit SVT by typing an empty line!\n";
 	if ( not ( -e $ARGV[0] ) )
 	{
-		print "File $ARGV[0] will be generated!\n\n";
+		print $OUT "File $ARGV[0] will be generated!\n\n";
 	} else
 	{
-		print "Warning! Vocabulary will be concatenated to file $ARGV[0]! To abort immediately, press ^C!\n\n";
+		print $OUT "Warning! Vocabulary will be concatenated to file $ARGV[0]! To abort immediately, press ^C!\n\n";
 	}
 
 	my @l1 = ( "000" );
@@ -335,8 +343,8 @@ if ( $mode[0] eq 'w' )
 	my $j = 1;
 	while ( $l1[$i-1] ne '' )
 	{
-		print " $j/l1  > ";
-		$l1[$i] = readnchomp();
+		$l1[$i] = $term->readline(" $j/l1  > ");
+		$term->addhistory($inp);
 
 		if ( not ( ( $l1[$i] =~ m/^#.*/ ) or ( $l1[$i] eq '' ) ) ) # if it isn't a comment and it isn't a terminate line (empty line), write a '=' at the end of line
 		{
@@ -351,10 +359,10 @@ if ( $mode[0] eq 'w' )
 		}
 		elsif ( ($l1[$i] ne '') and not( $l1[$i] =~ m/^#.*/ ) ) # if it isn't a comment and it isn't an empty line (terminate line), ask second column/2. language
 		{
-			
-			print " $j/l2  > ";
-			$l2[$i] = readnchomp();
-			print "\n";
+
+			$l2[$i] = $term->readline(" $j/l2  > ");
+			$term->addhistory($inp);
+			print $OUT "\n";
 
 		}
 		++$i;
@@ -362,18 +370,18 @@ if ( $mode[0] eq 'w' )
 	}
 	$j = 0;
 	$i -= 1; # else the last entry with '' is counted too
-	
-	print("\n$i records read!\n");
+
+	print $OUT "\n$i records read!\n";
 	open( my $writeFile, '>>', $ARGV[0]) or die("Open of $ARGV[0] not possible: $!");
-	
+
 	print $writeFile "### This file was created by SVocTrainer (c) 2010, 2011 Der Messer & LLynx\n";
 
 	for $j (0..($i-1))
 	{
-		print($writeFile "$l1[$j]" . "$l2[$j]" . "\n");
+		print $writeFile "$l1[$j]" . "$l2[$j]" . "\n";
 	}
-	
+
 	close $writeFile;
-	print "Vocabulary written succesful into $ARGV[0]!\n\n";
+	print $OUT "Vocabulary written succesful into $ARGV[0]!\n\n";
 }
 
